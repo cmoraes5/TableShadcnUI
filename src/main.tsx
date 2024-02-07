b78ec8bf7d0bb19038971ee1bb147a292d6b393e
Caio Moraes V Santos <caio.moraes@radixeng.com>
2024-02-07 00:11:16 -0300

    :sparkles: url state implemented

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,15 @@ import './global.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './lib/react-query.ts'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>,
 )
